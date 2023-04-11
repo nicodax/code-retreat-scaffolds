02d1e64e9063ff24934ee6558d4ea90f57c668d2
nicodax <nicodax@proton.me>
2023-04-11 17:44:48 +0200

    example: solving the gol in bash (infinite grid)

--- bash/display.sh
+++ bash/display.sh
@@ -3,15 +3,25 @@ DISPLAY_GRID_WIDTH=3
 INIT=(0 0 0 1 1 1 0 0 0)
 DISPLAY_GRID=()
 INFINITE_GRID=1
+declare -A LIVE_CELLS=()
+declare -A CELLS_TO_CHECK=()
 
 function init_display_grid_with_vertical_spinner {
   GRID_WIDTH=3
   DISPLAY_GRID=(0 1 0 0 1 0 0 1 0)
+  LIVE_CELLS=()
+  LIVE_CELLS["1_0"]=1
+  LIVE_CELLS["1_1"]=1
+  LIVE_CELLS["1_2"]=1
 }
 
 function init_display_grid_with_horizontal_spinner {
   DISPLAY_GRID_WIDTH=3
   DISPLAY_GRID=(0 0 0 1 1 1 0 0 0)
+  LIVE_CELLS=()
+  LIVE_CELLS["0_1"]=1
+  LIVE_CELLS["1_1"]=1
+  LIVE_CELLS["2_1"]=1
 }
 
 function print_border {
@@ -28,17 +38,24 @@ function print_row {
 }
 
 function next_gen_infinite_grid {
-  # TO DO
   declare -A next_live_cells
-  next_live_cells["1-0"]=1
-  next_live_cells["1-1"]=1
-  next_live_cells["1-2"]=1
-  
+
+  get_cells_to_check
+  for key in "${!CELLS_TO_CHECK[@]}"; do
+    IFS="_" read -r currentx currenty <<< "$key"
+    next_state=$(get_next_state_of $currentx $currenty)
+    if [[ $next_state -eq 1 ]]; then
+      next_live_cells["${currentx}_${currenty}"]=1
+    fi
+  done
+
   DISPLAY_GRID=()
+  LIVE_CELLS=()
   for (( y=0 ; y<$DISPLAY_GRID_WIDTH ; y++)); do
     for (( x=0 ; x<$DISPLAY_GRID_WIDTH ; x++)); do
-      if [[ ${next_live_cells["$x-$y"]} -eq 1 ]]; then
+      if [[ ${next_live_cells["${x}_${y}"]} -eq 1 ]]; then
         DISPLAY_GRID+=(1)
+        LIVE_CELLS["${x}_${y}"]=1
       else
         DISPLAY_GRID+=(0)
       fi
@@ -58,4 +75,57 @@ function next_gen {
   else
     next_gen_finite_grid
   fi
+}
+
+function get_neighbors_of {
+  local cell_x=$1
+  local cell_y=$2
+  local neighbors=()
+  for (( yn=(( $cell_y - 1 )) ; yn<=(( $cell_y + 1 )) ; yn++ )); do
+    for (( xn=(( $cell_x - 1 )) ; xn<=(( $cell_x + 1 )) ; xn++ )); do
+      if [[ ! ((xn -eq $cell_x && yn -eq $cell_y)) ]]; then
+        neighbors+=("${xn}_${yn}")
+      fi
+    done
+  done
+  echo "${neighbors[@]}"
+}
+
+function get_live_neighbor_count_of {
+  local cell_x=$1
+  local cell_y=$2
+  local neighbors=($(get_neighbors_of $cell_x $cell_y))
+  local live_neighbor_count=0
+  for neighbor in "${neighbors[@]}"; do
+    if [[ -v ${LIVE_CELLS["$neighbor"]} ]]; then
+      (( live_neighbor_count++ ))
+    fi
+  done
+  echo $live_neighbor_count
+}
+
+function get_next_state_of {
+  local x=$1
+  local y=$2
+  local live_neighbor_count=$(get_live_neighbor_count_of $x $y)
+  if [[ -v ${LIVE_CELLS["${x}_${y}"]} && $live_neighbor_count -ge 2 && $live_neighbor_count -le 3 ]]; then
+    echo 1
+  elif [[ ! (( -v ${LIVE_CELLS["${x}_${y}"]} )) && $live_neighbor_count -eq 3 ]]; then
+    echo 1
+  else
+    echo 0
+  fi
+}
+
+function get_cells_to_check {
+  CELLS_TO_CHECK=()
+  declare -A neighbors=()
+  for key in "${!LIVE_CELLS[@]}"; do
+    CELLS_TO_CHECK[$key]=1
+    IFS="_" read -r currentx currenty <<< "$key"
+    local current_neighbors=($(get_neighbors_of $currentx $currenty))
+    for neighbor in "${current_neighbors[@]}"; do
+      CELLS_TO_CHECK[$neighbor]=1
+    done
+  done
 }
--- bash/test_display.sh
+++ bash/test_display.sh
@@ -7,6 +7,11 @@ test_init_display_grid_with_vertical_spinner() {
     expected=(0 1 0 0 1 0 0 1 0)
     assertEquals 3 $DISPLAY_GRID_WIDTH
     assertEquals "${expected[*]}" "${DISPLAY_GRID[*]}"
+
+    assertEquals 3 "${#LIVE_CELLS[@]}"
+    assertEquals 1 "${LIVE_CELLS["1_0"]}"
+    assertEquals 1 "${LIVE_CELLS["1_1"]}"
+    assertEquals 1 "${LIVE_CELLS["1_2"]}"
 }
 
 # IT SHOULD CREATE A 3x3 GRID WITH A HORIZONTAL SPINNER
@@ -15,6 +20,11 @@ test_init_display_grid_with_horizontal_spinner() {
     expected=(0 0 0 1 1 1 0 0 0)
     assertEquals 3 $DISPLAY_GRID_WIDTH
     assertEquals "${expected[*]}" "${DISPLAY_GRID[*]}"
+
+    assertEquals 3 "${#LIVE_CELLS[@]}"
+    assertEquals 1 "${LIVE_CELLS["0_1"]}"
+    assertEquals 1 "${LIVE_CELLS["1_1"]}"
+    assertEquals 1 "${LIVE_CELLS["2_1"]}"
 }
 
 # IT SHOULD PRINT THE GRID BORDER DEPENDING ON THE GRID SIZE
@@ -56,6 +66,100 @@ test_print_row() {
     assertEquals "$expected" "$actual"
 
     rm "$tmpfile"
+}
+
+# IT SHOULD RETURN GIVEN CELL NEIGHBORS
+test_get_neighbors_of() {
+    init_display_grid_with_vertical_spinner
+
+    # CELL (0, 0)
+    expected_neighbors_00=("-1_-1" "0_-1" "1_-1" "-1_0" "1_0" "-1_1" "0_1" "1_1")
+    neighbors_00=($(get_neighbors_of 0 0))
+    assertEquals 8 "${#neighbors_00[@]}"
+    assertEquals "${expected_neighbors_00[*]}" "${neighbors_00[*]}"
+
+    # CELL (1, 0)
+    expected_neighbors_10=("0_-1" "1_-1" "2_-1" "0_0" "2_0" "0_1" "1_1" "2_1")
+    neighbors_10=($(get_neighbors_of 1 0))
+    assertEquals 8 "${#neighbors_10[@]}"
+    assertEquals "${expected_neighbors_10[*]}" "${neighbors_10[*]}"
+
+    # CELL (1, 1)
+    expected_neighbors_11=("0_0" "1_0" "2_0" "0_1" "2_1" "0_2" "1_2" "2_2")
+    neighbors_11=($(get_neighbors_of 1 1))
+    assertEquals 8 "${#neighbors_11[@]}"
+    assertEquals "${expected_neighbors_11[*]}" "${neighbors_11[*]}"
+}
+
+# IT SHOULD RETURN THE NUMBER OF ALIVE NEIGHBORS FOR GIVEN CELL
+test_get_live_neighbor_count_of() {
+    init_display_grid_with_vertical_spinner
+
+    # CELL (0, 0)
+    assertEquals 2 $(get_live_neighbor_count_of 0 0)
+
+    # CELL (1, 0)
+    assertEquals 1 $(get_live_neighbor_count_of 1 0)
+
+    # CELL (0, 1)
+    assertEquals 3 $(get_live_neighbor_count_of 0 1)
+
+    # CELL (1, 1)
+    assertEquals 2 $(get_live_neighbor_count_of 1 1)
+}
+
+# IT SHOULD RETURN THE NEXT STATE OF GIVEN CELL
+test_get_next_state_of() {
+    init_display_grid_with_vertical_spinner
+
+    # CELL (0, 0)
+    assertEquals 0 $(get_next_state_of 0 0)
+
+    # CELL (1, 0)
+    assertEquals 0 $(get_next_state_of 1 0)
+
+    # CELL (0, 1)
+    assertEquals 1 $(get_next_state_of 0 1)
+
+    # CELL (1, 1)
+    assertEquals 1 $(get_next_state_of 1 1)
+}
+
+# IT SHOULD RETURN THE NEXT GEN LIVE CELLS
+test_next_gen_infinite_grid() {
+    init_display_grid_with_vertical_spinner
+    next_gen_infinite_grid
+
+    expected=(0 0 0 1 1 1 0 0 0)
+    assertEquals "${expected[*]}" "${DISPLAY_GRID[*]}"
+
+    assertEquals 3 "${#LIVE_CELLS[@]}"
+    assertEquals 1 "${LIVE_CELLS["0_1"]}"
+    assertEquals 1 "${LIVE_CELLS["1_1"]}"
+    assertEquals 1 "${LIVE_CELLS["2_1"]}"
+}
+
+# IT SHOULD RETURN THE LIST OF CELLS TO CHECK
+test_get_cells_to_check() {
+    init_display_grid_with_vertical_spinner
+    get_cells_to_check
+
+    assertEquals 15 "${#CELLS_TO_CHECK[@]}"
+    assertEquals 1 "${CELLS_TO_CHECK["1_0"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["1_1"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["1_2"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["0_-1"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["1_-1"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["2_-1"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["0_0"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["2_0"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["0_1"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["2_1"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["0_2"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["2_2"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["0_3"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["1_3"]}"
+    assertEquals 1 "${CELLS_TO_CHECK["2_3"]}"
 }
 
 # LOAD SHUNIT2
