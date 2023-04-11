65a372bcb68ba972aade69933458bcec42ac9178
nicodax <nicodax@proton.me>
2023-04-11 10:09:25 +0200

    example: solving the gol in bash (finite grid)

--- bash/display.sh
+++ bash/display.sh
@@ -59,3 +59,49 @@ function next_gen {
     next_gen_finite_grid
   fi
 }
+
+function get_neighbors_of {
+  local cell_x=$1
+  local cell_y=$2
+  local startx=$(( cell_x > 0 ? cell_x - 1 : cell_x ))
+  local starty=$(( cell_y > 0 ? cell_y - 1 : cell_y ))
+  local endx=$(( cell_x < GRID_WIDTH-1 ? cell_x + 1 : cell_x ))
+  local endy=$(( cell_y < GRID_WIDTH-1 ? cell_y + 1 : cell_y ))
+  local neighbors=()
+  for (( zy=starty ; zy<=endy ; zy++ )); do
+    for (( zx=startx ; zx<=endx ; zx++ )); do
+      if (( zx == cell_x && zy == cell_y )); then
+        continue
+      fi
+      local z_idx=$(( (zy * GRID_WIDTH) + zx ))
+      neighbors+=("${DISPLAY_GRID[$z_idx]}")
+    done
+  done
+  echo "${neighbors[@]}"
+}
+
+function get_live_neighbor_count_from {
+  local neighbors=("$@")
+  local live_neighbor_count=0
+  for neighbor in "${neighbors[@]}"; do
+    if (( neighbor == 1 )); then
+      (( live_neighbor_count++ ))
+    fi
+  done
+  echo "$live_neighbor_count"
+}
+
+function get_next_state_of {
+  local cell_x=$1
+  local cell_y=$2
+  local cell_idx=$(( (cell_y * GRID_WIDTH) + cell_x ))
+  local neighbors=($(get_neighbors_of "$cell_x" "$cell_y"))
+  local live_neighbor_count=$(get_live_neighbor_count_from "${neighbors[@]}")
+  if (( DISPLAY_GRID[$cell_idx] == 1 && live_neighbor_count >= 2 && live_neighbor_count <= 3 )); then
+    echo 1
+  elif (( DISPLAY_GRID[$cell_idx] == 0 && live_neighbor_count == 3 )); then
+    echo 1
+  else
+    echo 0
+  fi
+}
--- bash/test_display.sh
+++ bash/test_display.sh
@@ -58,5 +58,85 @@ test_print_row() {
     rm "$tmpfile"
 }
 
+test_get_neighbors_of() {
+    init_display_grid_with_vertical_spinner
+
+    # CELL (0, 0)
+    expected_neighbours_00=(1 0 1)
+    actual_neighbours_00=$(get_neighbors_of 0 0)
+    assertEquals "${expected_neighbours_00[*]}" "${actual_neighbours_00[*]}"
+
+    # CELL (1, 0)
+    expected_neighbours_10=(0 0 0 1 0)
+    actual_neighbours_10=$(get_neighbors_of 1 0)
+    assertEquals "${expected_neighbours_10[*]}" "${actual_neighbours_10[*]}"
+
+    # CELL (1, 1)
+    expected_neighbours_11=(0 1 0 0 0 0 1 0)
+    actual_neighbours_11=$(get_neighbors_of 1 1)
+    assertEquals "${expected_neighbours_11[*]}" "${actual_neighbours_11[*]}"
+}
+
+test_get_live_neighbor_count_from() {
+    init_display_grid_with_vertical_spinner
+
+    # CELL (0, 0)
+    neighbours_00=($(get_neighbors_of 0 0))
+    expected_live_neighbours_00=2
+    actual_live_neighbours_00=$(get_live_neighbor_count_from "${neighbours_00[@]}")
+    assertEquals $expected_live_neighbours_00 $actual_live_neighbours_00
+
+    # CELL (1, 0)
+    neighbours_10=($(get_neighbors_of 1 0))
+    expected_live_neighbours_10=1
+    actual_live_neighbours_10=$(get_live_neighbor_count_from "${neighbours_10[@]}")
+    assertEquals $expected_live_neighbours_10 $actual_live_neighbours_10
+
+    # CELL (0, 1)
+    neighbours_01=($(get_neighbors_of 0 1))
+    expected_live_neighbours_01=3
+    actual_live_neighbours_01=$(get_live_neighbor_count_from "${neighbours_01[@]}")
+    assertEquals $expected_live_neighbours_01 $actual_live_neighbours_01
+
+    # CELL (1, 1)
+    neighbours_11=($(get_neighbors_of 1 1))
+    expected_live_neighbours_11=2
+    actual_live_neighbours_11=$(get_live_neighbor_count_from "${neighbours_11[@]}")
+    assertEquals $expected_live_neighbours_11 $actual_live_neighbours_11
+}
+
+test_get_next_state_of() {
+    init_display_grid_with_vertical_spinner
+
+    # CELL (0, 0)
+    expected_next_state_00=0
+    actual_next_state_00=$(get_next_state_of 0 0)
+    assertEquals $expected_next_state_00 $actual_next_state_00
+
+    # CELL (0, 1)
+    expected_next_state_01=1
+    actual_next_state_01=$(get_next_state_of 0 1)
+    assertEquals $expected_next_state_01 $actual_next_state_01
+
+    # CELL (1, 0)
+    expected_next_state_10=0
+    actual_next_state_10=$(get_next_state_of 1 0)
+    assertEquals $expected_next_state_10 $actual_next_state_10
+
+    # CELL (1, 1)
+    expected_next_state_11=1
+    actual_next_state_11=$(get_next_state_of 1 1)
+    assertEquals $expected_next_state_11 $actual_next_state_11
+}
+
+test_next_gen() {
+    init_display_grid_with_horizontal_spinner
+    expected_next_gen=(0 1 0 0 1 0 0 1 0)
+
+    next_gen
+
+    assertEquals "${expected_next_gen[*]}" "${DISPLAY_GRID[*]}"
+}
+
 # LOAD SHUNIT2
 . shunit2
