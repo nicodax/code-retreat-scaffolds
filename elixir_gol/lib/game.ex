5bb1551ab4ba423a54d8a3b757943c7a88a0965c
nicodax <nicodax@proton.me>
2023-04-18 17:19:04 +0200

    feat: first iteration

--- elixir_gol/lib/game.ex
+++ elixir_gol/lib/game.ex
@@ -3,6 +3,14 @@ defmodule Game do
   Documentation for `Game`.
   """
 
+  @typedoc """
+  a cell with x and y coordinates
+  """
+  @type cell_t :: %{
+    x: integer,
+    y: integer
+  }
+
   @doc """
   Hello world.
 
@@ -14,5 +22,143 @@ defmodule Game do
   """
   def hello do
     :world
+  end
+
+  @doc """
+  Gets given cell neighbors
+
+  ## Examples
+
+      iex> Game.get_neighbors_of(%{x: 0, y: 0})
+      [
+        %{x: -1, y: -1},
+        %{x: 0, y: -1},
+        %{x: 1, y: -1},
+        %{x: -1, y: 0},
+        %{x: 1, y: 0},
+        %{x: -1, y: 1},
+        %{x: 0, y: 1},
+        %{x: 1, y: 1}
+      ]
+  """
+  @spec get_neighbors_of(cell_t) :: [cell_t]
+  def get_neighbors_of(cell) do
+    loop_over_neighbors([], cell.x - 1, cell.y - 1, cell.x + 1, cell.y + 1)
+  end
+  
+  @spec loop_over_neighbors([cell_t], integer, integer, integer, integer) :: [cell_t]
+  defp loop_over_neighbors(neighbors_prev, x, y, maxX, maxY) when x <= maxX and y <= maxY do
+    neighbors = neighbors_prev ++ [%{x: x, y: y}]
+    {x, y} = increment_x_y_counters(x, y, maxX, maxY)
+    loop_over_neighbors(neighbors, x, y, maxX, maxY)
+  end
+  
+  defp loop_over_neighbors(neighbors, _x, _y, maxX, maxY) do
+    List.delete(neighbors, %{x: maxX - 1, y: maxY - 1})
+  end
+  
+  @spec increment_x_y_counters(integer, integer, integer, integer) :: {integer, integer}
+  defp increment_x_y_counters(x, y, maxX, maxY) do
+    cond do
+      x == maxX ->
+        {x - 2, y + 1}
+      x == maxX and y == maxY ->
+        {x, y}
+      true ->
+        {x + 1, y}
+    end
+  end
+
+  @doc """
+  Gets given cell live neighbors count
+
+  ## Examples
+
+      iex> Game.get_live_neighbors_of(%{x: 0, y: 0}, [%{x: 1, y: 0}, %{x: 1, y: 1}, %{x: 1, y: 2}])
+      2
+  """
+  @spec get_live_neighbors_of(cell_t, [cell_t]) :: integer
+  def get_live_neighbors_of(cell, live_cells) do
+    neighbors = get_neighbors_of(cell)
+    alive? = &(Enum.member?(live_cells, &1))
+    live_neighbors = Enum.filter(neighbors, alive?)
+    length(live_neighbors)
+  end
+
+  @doc """
+  Gets given cell's next based of gol's rules
+  ## Examples
+
+      iex> Game.get_next_state_of(%{x: 0, y: 0}, [%{x: 1, y: 0}, %{x: 1, y: 1}, %{x: 1, y: 2}])
+      false
+  """
+  @spec get_next_state_of(cell_t, [cell_t]) :: boolean
+  def get_next_state_of(cell, live_cells) do
+    live_neighbor_count = get_live_neighbors_of(cell, live_cells)
+    alive? = &(Enum.member?(live_cells, &1))
+    cond do
+      alive?.(cell) and live_neighbor_count >= 2 and live_neighbor_count <= 3 ->
+        true
+      not alive?.(cell) and live_neighbor_count == 3 ->
+        true
+      true ->
+        false
+    end
+  end
+
+  @doc """
+  Gets cells to check for the next generation
+  ## Examples
+
+      iex> Game.get_cells_to_check([%{x: 1, y: 0}, %{x: 1, y: 1}, %{x: 1, y: 2}])
+      [
+        %{x: 0, y: -1},
+        %{x: 1, y: -1},
+        %{x: 2, y: -1},
+        %{x: 0, y: 0},
+        %{x: 1, y: 0},
+        %{x: 2, y: 0},
+        %{x: 0, y: 1},
+        %{x: 1, y: 1},
+        %{x: 2, y: 1},
+        %{x: 0, y: 2},
+        %{x: 1, y: 2},
+        %{x: 2, y: 2},
+        %{x: 0, y: 3},
+        %{x: 1, y: 3},
+        %{x: 2, y: 3},
+      ]
+  """
+  @spec get_cells_to_check([cell_t]) :: [cell_t]
+  def get_cells_to_check(live_cells) do
+    live_cells_neighbors_duplicates = Enum.reduce(live_cells, [], fn(cell, acc) -> acc ++ get_neighbors_of(cell) end)
+    cells_to_check_duplicates = live_cells_neighbors_duplicates ++ live_cells
+    cells_to_check = Enum.uniq(cells_to_check_duplicates)
+    compare_cells = fn cell1, cell2 ->
+      cond do
+        cell1.y != cell2.y ->
+          cell1.y < cell2.y
+        true ->
+          cell1.x < cell2.x
+      end
+    end
+    Enum.sort(cells_to_check, compare_cells)
+  end
+
+  @doc """
+  Gets game next generation
+  ## Examples
+
+      iex> Game.get_next_gen([%{x: 1, y: 0}, %{x: 1, y: 1}, %{x: 1, y: 2}])
+      [
+        %{x: 0, y: 1},
+        %{x: 1, y: 1},
+        %{x: 2, y: 1}
+      ]
+  """
+  @spec get_next_gen([cell_t]) :: [cell_t]
+  def get_next_gen(live_cells) do
+    cells_to_check = get_cells_to_check(live_cells)
+    Enum.filter(cells_to_check, fn cell -> get_next_state_of(cell, live_cells) end)
   end
 end
--- elixir_gol/test/game_test.exs
+++ elixir_gol/test/game_test.exs
@@ -5,4 +5,74 @@ defmodule GameTest do
   test "greets the world" do
     assert Game.hello() == :world
   end
+
+  test "get neighbors of given cell" do
+    neighbors = Game.get_neighbors_of(%{x: 0, y: 0})
+    assert length(neighbors) == 8
+    assert Enum.member?(neighbors, %{x: -1, y: -1})
+    assert Enum.member?(neighbors, %{x: 0, y: -1})
+    assert Enum.member?(neighbors, %{x: 1, y: -1})
+    assert Enum.member?(neighbors, %{x: -1, y: 0})
+    assert Enum.member?(neighbors, %{x: 1, y: 0})
+    assert Enum.member?(neighbors, %{x: -1, y: 1})
+    assert Enum.member?(neighbors, %{x: 0, y: 1})
+    assert Enum.member?(neighbors, %{x: 1, y: 1})
+  end
+
+  test "get live neighbor count" do
+    live_cells = [%{x: 1, y: 0}, %{x: 1, y: 1}, %{x: 1, y: 2}]
+
+    live_neighbors00 = Game.get_live_neighbors_of(%{x: 0, y: 0}, live_cells)
+    assert live_neighbors00 == 2
+
+    live_neighbors10 = Game.get_live_neighbors_of(%{x: 1, y: 0}, live_cells)
+    assert live_neighbors10 == 1
+
+    live_neighbors01 = Game.get_live_neighbors_of(%{x: 0, y: 1}, live_cells)
+    assert live_neighbors01 == 3
+
+    live_neighbors11 = Game.get_live_neighbors_of(%{x: 1, y: 1}, live_cells)
+    assert live_neighbors11 == 2
+  end
+
+  test "get next state of given cell" do
+    live_cells = [%{x: 1, y: 0}, %{x: 1, y: 1}, %{x: 1, y: 2}]
+
+    assert not Game.get_next_state_of(%{x: 0, y: 0}, live_cells)
+    assert not Game.get_next_state_of(%{x: 1, y: 0}, live_cells)
+    assert Game.get_next_state_of(%{x: 0, y: 1}, live_cells)
+    assert Game.get_next_state_of(%{x: 1, y: 1}, live_cells)
+  end
+
+  test "get cells to check for next state" do
+    live_cells = [%{x: 1, y: 0}, %{x: 1, y: 1}, %{x: 1, y: 2}]
+    cells_to_check = Game.get_cells_to_check(live_cells)
+
+    assert length(cells_to_check) == 15
+    assert Enum.member?(cells_to_check, %{x: 0, y: -1})
+    assert Enum.member?(cells_to_check, %{x: 1, y: -1})
+    assert Enum.member?(cells_to_check, %{x: 2, y: -1})
+    assert Enum.member?(cells_to_check, %{x: 0, y: 0})
+    assert Enum.member?(cells_to_check, %{x: 1, y: 0})
+    assert Enum.member?(cells_to_check, %{x: 2, y: 0})
+    assert Enum.member?(cells_to_check, %{x: 0, y: 1})
+    assert Enum.member?(cells_to_check, %{x: 1, y: 1})
+    assert Enum.member?(cells_to_check, %{x: 2, y: 1})
+    assert Enum.member?(cells_to_check, %{x: 0, y: 2})
+    assert Enum.member?(cells_to_check, %{x: 1, y: 2})
+    assert Enum.member?(cells_to_check, %{x: 2, y: 2})
+    assert Enum.member?(cells_to_check, %{x: 0, y: 3})
+    assert Enum.member?(cells_to_check, %{x: 1, y: 3})
+    assert Enum.member?(cells_to_check, %{x: 2, y: 3})
+  end
+
+  test "get game next generation" do
+    live_cells = [%{x: 1, y: 0}, %{x: 1, y: 1}, %{x: 1, y: 2}]
+    next_gen = Game.get_next_gen(live_cells)
+
+    assert length(next_gen) == 3
+    assert Enum.member?(next_gen, %{x: 0, y: 1})
+    assert Enum.member?(next_gen, %{x: 1, y: 1})
+    assert Enum.member?(next_gen, %{x: 2, y: 1})
+  end
 end
